4ab59743febbe78c13d6c6f4bc1803ef063af882
klaasMG <klaasyoutube02@outlook.com>
2025-12-11 07:36:13 +0100

    now getting the position of a widget is cpu side

--- the_ui_tree_build/assets/shaders/basic_vert.glsl
+++ the_ui_tree_build/assets/shaders/basic_vert.glsl
@@ -6,10 +6,28 @@ layout(location = 1) in vec2 aUV;
 out vec2 vUV;
 flat out int vIndex;
 flat out Widget widget;
+
+layout(std430, binding = 0) buffer Position {
+    int widget_pos[];
+};
+
+layout(std430, binding = 8) buffer Parent {
+    int widget_parent[];
+};
+
+int get_position(int index,int offset){
+    int position = widget_pos[index * 6 + offset];
+    int parent = widget_parent[index];
+    while (parent != 0){
+        position += widget_pos[parent * 6 + offset];
+        parent = widget_parent[parent];
+    }
+    return position;
+}
 void main() {
+    vIndex = gl_VertexID;
     Widget w = Widget(ivec3(1,1,1),ivec3(1,1,1),1,ivec4(1,1,1,1),2, 3);
     widget = w;
-    vIndex = gl_VertexID;
     vUV = aUV;
     gl_Position = vec4(aPos, 0.0, 1.0);
 }
