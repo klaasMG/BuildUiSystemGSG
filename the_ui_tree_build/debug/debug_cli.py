6961fb953f959c5909f7af6cdc3234a780659cd0
klaasMG <klaasyoutube02@outlook.com>
2025-12-21 18:04:52 +0100

    now getting the position of a widget is cpu side

--- the_ui_tree_build/debug/debug_cli.py
+++ the_ui_tree_build/debug/debug_cli.py
@@ -5,28 +5,51 @@ from enum import Enum
 
 class DebugData(Enum):
     DebugEnabled = "debug_enabled"
+    Create = "create"
+    SetDebug = "set_debug"
+    Exit = "exit"
 
-flags = sys.argv[1:]
-debug_config = flags[0]
-set_debug_config = flags[1].lower() == "true" if len(flags) > 1 else None
+def repl():
+    running = True
+    while running:
+        try:
+            cmd = input("work> ").strip()
+        except EOFError:
+            break
 
-DEBUG_FILE = Path(f"{debug_config}.json")  # je debug content file
+        if cmd in ("exit", "quit"):
+            break
 
-def debug_run():
-    if not DEBUG_FILE.exists():
-        data = { DebugData.DebugEnabled.value: False }
-        with open(DEBUG_FILE, "w") as f:
-            json.dump(data, f, indent=2)
-
-    with open(DEBUG_FILE, "r") as f:
-        data = json.load(f)
-
-    # update alleen als set_debug_config niet None is
-    if set_debug_config is not None:
-        data[DebugData.DebugEnabled.value] = set_debug_config
-
-    with open(DEBUG_FILE, "w") as f:
-        json.dump(data, f, indent=4)
+        if not cmd:
+            continue
+            
+        cmd_list = cmd.split()
+        first_cmd = cmd_list[0]
+        
+        if first_cmd == DebugData.Create.value:
+            debug_config = cmd_list[1]
+            DEBUG_FILE = Path(f"{debug_config}.json")
+            data = {DebugData.DebugEnabled.value: False}
+            with open(DEBUG_FILE, "w") as f:
+                json.dump(data, f, indent=4)
+            
+        elif first_cmd == DebugData.SetDebug.value:
+            debug_config = cmd_list[1]
+            DEBUG_FILE = Path(f"{debug_config}.json")
+            with open(DEBUG_FILE, "r") as f:
+                data = json.load(f)
+            
+            set_debug_config = cmd_list[2].lower() == "true"
+            # update alleen als set_debug_config niet None is
+            if set_debug_config is not None:
+                data[DebugData.DebugEnabled.value] = set_debug_config
+            
+            with open(DEBUG_FILE, "w") as f:
+                json.dump(data, f, indent=4)
+        elif first_cmd == DebugData.Exit.value:
+            running = False
+        else:
+            print(f"unknown command: {cmd}")
 
 if __name__ == "__main__":
-    debug_run()
+    repl()
