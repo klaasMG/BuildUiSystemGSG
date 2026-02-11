dd93c9b51ea7e49d524f3166103e01860e079c43
klaasMG <klaasyoutube02@outlook.com>
2026-02-11 16:31:19 +0100

    GMake is now fully supported. it can do includes and extend the standard. I may still add global constant to it and after that global constants that are given a value at the start of runtime through the console. after that maybe add it the runtime constant from a file.

--- the_code/build_dependecy_graph.cpp
+++ the_code/build_dependecy_graph.cpp
@@ -3,101 +3,25 @@
 //
 #include "TokeniserGMAKE.h"
 #include "SimpleASTGMAKE.h"
+#include "GMAKE_EXEPTION.h"
+#include "GMakeTypes.h"
+#include "file_utils.h"
 #include <iostream>
 #include <string>
 #include <fstream>
 #include <map>
 #include <memory>
 #include <ranges>
-#include <unordered_map>
-#define PRINT(text) cout << text << endl
+#define PRINT(text) std::cout << text << std::endl
 
-using namespace std;
 namespace fs = std::filesystem;
 
 fs::path current_dir;
+bool debug = false;
+GMAKE_EXCEPTION ExceptionHandler = GMAKE_EXCEPTION{debug};
 
-enum class GMakeFunction {
-	SET_PROJECT_DIRECTORY,
-	SET_PROGRAM,
-	EXTEND_STANDARD,
-	UNKNOWN
-};
-
-struct GMAKEConfig {
-	fs::path ProjectDir;
-	map<string, vector<fs::path>> ShaderPrograms;
-    vector<fs::path> StandardExtensions;
-};
-
-string insertLine(std::string text, int lineIndex, const std::string& newLine){
-    size_t pos = 0;
-    int line = 0;
-
-    while (line < lineIndex && pos != std::string::npos)
-    {
-        pos = text.find('\n', pos);
-
-        if (pos != std::string::npos)
-            pos++; // move past '\n'
-
-        line++;
-    }
-
-    if (pos == std::string::npos)
-        text += newLine + "\n";   // append if too short
-    else
-        text.insert(pos, newLine + "\n");
-
-    return text;
-}
-
-string readFile(const char* path) {
-	ifstream file(path, ios::binary);
-	if (!file) return "";
-
-	file.seekg(0, ios::end);
-	size_t size = file.tellg();
-	file.seekg(0);
-
-	string data(size, '\0');
-	file.read(data.data(), size);
-
-	return data;
-}
-
-void WriteFile(const fs::path& filepath, const std::string& content) {
-	std::ofstream file(filepath);
-	file << content;
-	file.close();  // Optional, destructor closes it
-}
-
-string ReadFilePath(const fs::path& path) {
-	ifstream file(path, ios::binary);
-	if (!file) return "";
-
-	file.seekg(0, ios::end);
-	size_t size = file.tellg();
-	file.seekg(0);
-
-	string data(size, '\0');
-	file.read(data.data(), size);
-
-	return data;
-}
-
-GMakeFunction parseFunction(const string& name) {
-	static const unordered_map<string, GMakeFunction> functionMap = {
-		{"SetProjectDirectory", GMakeFunction::SET_PROJECT_DIRECTORY},
-		{"SetProgram", GMakeFunction::SET_PROGRAM},
-	    {"ExtendStandard", GMakeFunction::EXTEND_STANDARD}
-	};
-
-	unordered_map<string, GMakeFunction>::const_iterator it = functionMap.find(name);
-	return (it != functionMap.end()) ? it->second : GMakeFunction::UNKNOWN;
-}
-
-GMAKEConfig runGMAKEFunction(const string& function_name, const vector<string>& function_args, GMAKEConfig config) {
+GMAKEConfig runGMAKEFunction(const std::string& function_name, const std::vector<std::string>& function_args, GMAKEConfig config) {
+    ExceptionHandler.add_to_call_stack(function_name);
 	switch (parseFunction(function_name)) {
 	case GMakeFunction::SET_PROJECT_DIRECTORY: {
 			fs::path project_dir = function_args[0];
@@ -112,9 +36,9 @@ GMAKEConfig runGMAKEFunction(const string& function_name, const vector<string>& 
 	}
 
 	case GMakeFunction::SET_PROGRAM: {
-			const string& shader_program = function_args[0];
-			vector<fs::path> shaders;
-			for (const string& arg : function_args | std::views::drop(1)) {
+			const std::string& shader_program = function_args[0];
+			std::vector<fs::path> shaders;
+			for (const std::string& arg : function_args | std::views::drop(1)) {
 				fs::path path_arg = arg;
 				shaders.emplace_back(path_arg);
 			}
@@ -123,47 +47,47 @@ GMAKEConfig runGMAKEFunction(const string& function_name, const vector<string>& 
 	}
 
 	case GMakeFunction::EXTEND_STANDARD:{
-	    for (const string arg : function_args){
+	    for (const std::string arg : function_args){
 	        config.StandardExtensions.emplace_back(arg);
 	    }
 	    break;
 	}
 
 	case GMakeFunction::UNKNOWN:
-		throw runtime_error("Unknown function");
+		ExceptionHandler.send(1, "Function is not found" + function_name);
 		break;
 	}
 
 	return config;
 }
 
-vector<unique_ptr<ASTNode>> build_ast(const string& gmake_file){
+std::vector<std::unique_ptr<ASTNode>> build_ast(const std::string& gmake_file){
 	TokeniserGMAKE tokeniser(gmake_file);
-	vector<Token> tokens = tokeniser.Tokenise();
+	std::vector<Token> tokens = tokeniser.Tokenise();
 	ASTGMAKE ast_builder(tokens);
-	vector<unique_ptr<ASTNode>> nodes = ast_builder.getNodes();
+	std::vector<std::unique_ptr<ASTNode>> nodes = ast_builder.getNodes();
 	return nodes;
 }
 
-string do_includes(const string& shader, map<fs::path, string>& open_shaders){
-	istringstream stream(shader);
-	string line;
-	string rebuild;
+std::string do_includes(const std::string& shader, std::map<fs::path, std::string>& open_shaders){
+	std::istringstream stream(shader);
+	std::string line;
+	std::string rebuild;
 	while (getline(stream, line)){
-		string new_line;
+		std::string new_line;
 		if (line.starts_with("#include")) {
 			// Extract the filename from #include "filename" or #include <filename>
 			size_t first_quote = line.find('"');
 			size_t last_quote = line.rfind('"');
 
 			// Handle both "filename" and <filename> formats
-			if (first_quote == string::npos) {
+			if (first_quote == std::string::npos) {
 				first_quote = line.find('<');
 				last_quote = line.rfind('>');
 			}
 
-			if (first_quote != string::npos && last_quote != string::npos && first_quote != last_quote) {
-				string include_path = line.substr(first_quote + 1, last_quote - first_quote - 1);
+			if (first_quote != std::string::npos && last_quote != std::string::npos && first_quote != last_quote) {
+				std::string include_path = line.substr(first_quote + 1, last_quote - first_quote - 1);
 				fs::path shader_path(include_path);
 
 				if (shader_path.is_absolute()){
@@ -189,8 +113,8 @@ string do_includes(const string& shader, map<fs::path, string>& open_shaders){
 }
 
 void include_run(const fs::path& shader_directory, const GMAKEConfig &config){
-	map<fs::path, string> open_shader_files;
-	map<fs::path, string> open_include_files;
+	std::map<fs::path, std::string> open_shader_files;
+	std::map<fs::path, std::string> open_include_files;
 
 	// Create GMakeDir in the current directory
 	fs::path new_dir = config.ProjectDir.parent_path() / "GMakeDir";
@@ -198,8 +122,8 @@ void include_run(const fs::path& shader_directory, const GMAKEConfig &config){
 		fs::create_directory(new_dir);
 	}
 
-	for (const pair<const string, vector<fs::path>>& shader : config.ShaderPrograms) {
-		vector<fs::path> shaders = shader.second;
+	for (const std::pair<const std::string, std::vector<fs::path>>& shader : config.ShaderPrograms) {
+		std::vector<fs::path> shaders = shader.second;
 		for (const fs::path &file : shaders){
 			fs::path actual_file_path;
 
@@ -211,13 +135,13 @@ void include_run(const fs::path& shader_directory, const GMAKEConfig &config){
 			}
 
 			// Read and process the shader
-			string shader_content = ReadFilePath(actual_file_path);
+			std::string shader_content = ReadFilePath(actual_file_path);
 		    for (const fs::path& standard_path : config.StandardExtensions){
-		        string path_string = standard_path.string();
-		        string standard_file_path_include = "#include " + path_string;
+		        std::string path_string = standard_path.string();
+		        std::string standard_file_path_include = "#include " + path_string;
 		        shader_content = insertLine(shader_content, 1, standard_file_path_include);
 		    }
-			string included_shader = do_includes(shader_content, open_shader_files);
+			std::string included_shader = do_includes(shader_content, open_shader_files);
 
 			// Create output path in GMakeDir
 			fs::path output_file = new_dir / file.filename();
@@ -227,14 +151,14 @@ void include_run(const fs::path& shader_directory, const GMAKEConfig &config){
 	}
 
 	// Write all processed files
-	for (const pair<const fs::path, string> &write_file : open_include_files) {
+	for (const std::pair<const fs::path, std::string> &write_file : open_include_files) {
 		PRINT("Writing to: " << write_file.first);
 		WriteFile(write_file.first, write_file.second);
 	}
 }
 
-vector<string> make_args(const vector<IdentNode>& args){
-	vector<string> arg_string;
+std::vector<std::string> make_args(const std::vector<IdentNode>& args){
+	std::vector<std::string> arg_string;
 	for (const IdentNode& arg : args){
 		arg_string.push_back(arg.Ident);
 	}
@@ -244,27 +168,27 @@ vector<string> make_args(const vector<IdentNode>& args){
 int main(int argc, char* argv[]) {
 	if (argc == 2){
 		current_dir = fs::current_path();
-		cout << current_dir << endl;
+		std::cout << current_dir << std::endl;
 		char* gmake_file_path = argv[1];
-		string gmake_file = readFile(gmake_file_path);
-		vector<unique_ptr<ASTNode>> nodes = build_ast(gmake_file);
+		std::string gmake_file = readFile(gmake_file_path);
+		std::vector<std::unique_ptr<ASTNode>> nodes = build_ast(gmake_file);
 		GMAKEConfig config = GMAKEConfig();
 
-		for (const unique_ptr<ASTNode>& node : nodes){
+		for (const std::unique_ptr<ASTNode>& node : nodes){
 			if (dynamic_cast<FunctionNode*>(node.get())){
 				auto function = dynamic_cast<FunctionNode*>(node.get());
 				IdentNode function_name = function->Ident;
-				string name_check = function_name.Ident;
-				vector<IdentNode> function_args = function->Args;
-				vector<string> Args = make_args(function_args);
+				std::string name_check = function_name.Ident;
+				std::vector<IdentNode> function_args = function->Args;
+				std::vector<std::string> Args = make_args(function_args);
 				config = runGMAKEFunction(name_check, Args, config);
 			}
 		}
-		cout << config.ProjectDir << endl;
+		std::cout << config.ProjectDir << std::endl;
 		include_run("path", config);
 	}
 	else{
-		cout << "wrong number of arguments" << endl;
+		std::cout << "wrong number of arguments" << std::endl;
 	}
 
 	return 0;
