d150a8ef4f51c1d91dbd923f20702dfe61754e00
klaasMG <klaasyoutube02@outlook.com>
2026-02-24 00:39:59 +0100

    GMake is now fully supported. it can do includes and extend the standard. I may still add global constant to it and after that global constants that are given a value at the start of runtime through the console. after that maybe add it the runtime constant from a file.

--- the_ui_tree_build/FontManager.py
+++ the_ui_tree_build/FontManager.py
@@ -10,21 +10,49 @@ ttf_path = Path("assets/fonts/AovelSansRounded-rdDL.ttf")
 class FontManager:
     def __init__(self):
         self.font_map_image = Image.new("L",(8192,8912),0)
-        #self.font_map = Texture(self.font_map_image,"uFontMap", TextureType.GREY_SCALE)
+        self.font_map = Texture(self.font_map_image,"uFontMap", TextureType.GREY_SCALE)
         self.fonts: dict[str, Font] = {}
-        
-        
-    def render_text(self, text: str, font: str ,text_height: int):
+    
+    from PIL import Image
+    
+    def render_text(self, text: str, font: str, text_height: int):
         render_font: Font = self.fonts[font]
+        
+        # First pass: compute total width using advances
         text_length = 0
-        for i in text:
-            render_info_1 = render_font.get_render_info(text_height, ord(i))
-            advance = render_info_1[0]
+        for char in text:
+            render_info = render_font.get_render_info(text_height, ord(char))
+            advance = render_info[0]
             text_length += advance
-        text_image = Image.new("L",(text_length, text_height), 0)
-        for i in text:
-            rendered_char, rendered_info = self.render_char(i, render_font, text_height)
-            char_image = Image.fromarray(rendered_char,"L")
+        
+        # Create image tall enough for the line
+        text_image = Image.new("L", (text_length, text_height), 0)
+        
+        # Compute baseline (top + ascent)
+        if len(text) > 0:
+            # get ascent from first char (all glyphs share font ascent)
+            _, _, scaledAscent, scaledDescent, _ = render_font.get_render_info(text_height, ord(text[0]))
+        else:
+            scaledAscent = text_height
+        baseline_y = scaledAscent
+        
+        cursor_x = 0
+        for char in text:
+            rendered_char, rendered_info = self.render_char(char, render_font, text_height)
+            char_image = Image.fromarray(rendered_char, "L")
+            
+            advance, lsb, scaledAscent, scaledDescent, lineGap = rendered_info
+            # y0 = char bitmap offset relative to baseline
+            y0 = rendered_info[2] - scaledAscent  # or get from glyph bitmap box if available
+            
+            # Position: x = cursor + lsb, y = baseline + y0
+            text_image.paste(char_image, (cursor_x + lsb, baseline_y + y0))
+            
+            cursor_x += advance  # move pen
+        return text_image
+    
+    def update_texture(self):
+        self.font_map.resend(self.font_map_image)
         
     def add_font(self, font_name: str, font_file: Path):
         font_file = str(font_file)
@@ -47,3 +75,4 @@ class FontManager:
     
 f = FontManager()
 f.add_font("font",ttf_path)
+f.render_text("lk","font", 45)
