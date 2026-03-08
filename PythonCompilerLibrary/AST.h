89e752362e4f29c86c3127dd6f37d9e0a49709de
klaasMG <klaasyoutube02@outlook.com>
2026-03-08 15:57:34 +0100

    added multipule cmake to the system

---
+++ PythonCompilerLibrary/AST.h
@@ -0,0 +1,26 @@
+//
+// Created by klaas on 3/8/2026.
+//
+
+#ifndef SUPERBUILD_AST_H
+#define SUPERBUILD_AST_H
+
+struct NumberNode;
+struct StringNode;
+struct CallExprNode;
+struct ExprNode;
+struct AssignNode;
+struct FunctionNode;
+struct ClassNode;
+struct IdentNode;
+struct BinaryExprNode;
+struct UnaryExprNode;
+struct BinaryNode;
+struct IfNode;
+struct WhileNode;
+struct ForNode;
+struct MatchNode;
+struct TryNode;
+
+
+#endif //SUPERBUILD_AST_H
