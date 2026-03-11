3a61f78cbe79847c895cdcc85cd91c35cced4d0a
klaasMG <klaasyoutube02@outlook.com>
2026-03-11 23:02:03 +0100

    hooks still broken but working as intended for now it works this way fine

--- tooling/mirror/MirrorMain.py
+++ tooling/mirror/MirrorMain.py
@@ -16,16 +16,18 @@ def get_project_root(project: str)-> str:
 
 def copy_project():
     for project in mirror_projects:
-        proj_root = get_project_root(project)
-        proj_root_path = Path(proj_root)
-        proj_root_path = proj_root_path.absolute()
+        proj_root_path = Path(get_project_root(project))
         mirror_repo_base_use = Path(mirror_repo_base) / Path(f"{project}")
         mirror_repo_base_use_src = Path(mirror_repo_base_use) / Path(f"{project}")
+        
         if mirror_repo_base_use_src.exists():
             shutil.rmtree(mirror_repo_base_use_src)
+            
         shutil.copytree(proj_root_path, mirror_repo_base_use_src)
+        
         if not os.path.isdir(os.path.join(mirror_repo_base_use, ".git")):
             Repo.init(mirror_repo_base_use)
+            
         repo = Repo(mirror_repo_base_use)
         repo.git.add(".")
         repo.index.commit("commit")
