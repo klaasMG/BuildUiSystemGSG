c2bcf7814ff18aa3b639545183d03046942eba29
klaasMG <klaasyoutube02@outlook.com>
2026-03-12 00:17:49 +0100

    hooks still broken but working as intended for now it works this way fine

--- tooling/mirror/MirrorMain.py
+++ tooling/mirror/MirrorMain.py
@@ -3,20 +3,36 @@ import json
 import shutil
 from pathlib import Path
 from git import Repo
+from dotenv import load_dotenv
+from github import Github, Auth
+load_dotenv()
+GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
+github_client = Github(auth=Auth.Token(GITHUB_TOKEN))
+base_dir = os.path.dirname(os.path.abspath(__file__))
+os.chdir(base_dir)
 
 mirror_repo_base = Path(__file__).parent.parent.parent
 mirror_repo_base = Path(mirror_repo_base) / Path("public_mirror_repository's")
 
-json_file = open("tooling/mirror/mirror.json", "r")
+json_file = open("mirror.json", "r")
 mirror_repo = json.load(json_file)
 mirror_projects = mirror_repo["projects"]
+
+def resolve_back(path_str: str, base: Path = Path(__file__).parent) -> Path:
+    path = base
+    for part in path_str.split("/"):
+        if part == "..":
+            path = path.parent
+        elif part and part != ".":
+            path = path / part
+    return path.resolve()
 
 def get_project_root(project: str)-> str:
     return mirror_projects[project]["project_root"]
 
 def copy_project():
     for project in mirror_projects:
-        proj_root_path = Path(get_project_root(project))
+        proj_root_path = resolve_back(mirror_projects[project]["project_root"])
         mirror_repo_base_use = Path(mirror_repo_base) / Path(f"{project}")
         mirror_repo_base_use_src = Path(mirror_repo_base_use) / Path(f"{project}")
         
