9af02ea27716f0d1c34f2daac41bec5e25ddbbfd
klaasMG <klaasyoutube02@outlook.com>
2026-03-12 07:26:26 +0100

    hooks still broken but working as intended for now it works this way fine

--- tooling/mirror/MirrorMain.py
+++ tooling/mirror/MirrorMain.py
@@ -5,9 +5,11 @@ from pathlib import Path
 from git import Repo
 from dotenv import load_dotenv
 from github import Github, Auth
+
 load_dotenv()
 GITHUB_TOKEN = os.getenv("GITHUB_TOKEN")
 github_client = Github(auth=Auth.Token(GITHUB_TOKEN))
+
 base_dir = os.path.dirname(os.path.abspath(__file__))
 os.chdir(base_dir)
 
@@ -18,6 +20,7 @@ json_file = open("mirror.json", "r")
 mirror_repo = json.load(json_file)
 mirror_projects = mirror_repo["projects"]
 
+
 def resolve_back(path_str: str, base: Path = Path(__file__).parent) -> Path:
     path = base
     for part in path_str.split("/"):
@@ -27,30 +30,59 @@ def resolve_back(path_str: str, base: Path = Path(__file__).parent) -> Path:
             path = path / part
     return path.resolve()
 
-def get_project_root(project: str)-> str:
+
+def get_project_root(project: str) -> str:
     return mirror_projects[project]["project_root"]
+
 
 def copy_project():
     for project in mirror_projects:
         proj_root_path = resolve_back(mirror_projects[project]["project_root"])
-        mirror_repo_base_use = Path(mirror_repo_base) / Path(f"{project}")
-        mirror_repo_base_use_src = Path(mirror_repo_base_use) / Path(f"{project}")
-        
+
+        mirror_repo_base_use = Path(mirror_repo_base) / Path(project)
+        mirror_repo_base_use_src = Path(mirror_repo_base_use) / Path(project)
+
         if mirror_repo_base_use_src.exists():
             shutil.rmtree(mirror_repo_base_use_src)
-            
+
         shutil.copytree(proj_root_path, mirror_repo_base_use_src)
-        
+
         if not os.path.isdir(os.path.join(mirror_repo_base_use, ".git")):
             Repo.init(mirror_repo_base_use)
-            
+
+        git_user = github_client.get_user()
+
+        # get repo if it exists, otherwise create it
+        try:
+            repo_remote = git_user.get_repo(project)
+        except:
+            repo_remote = git_user.create_repo(project)
+
+        url = repo_remote.clone_url.replace(
+            "https://",
+            f"https://{GITHUB_TOKEN}@"
+        )
+
         repo = Repo(mirror_repo_base_use)
-        repo.git.add(".")
-        repo.index.commit("commit")
-        
-    
+
+        if repo.is_dirty(untracked_files=True):
+            repo.git.add(".")
+            repo.index.commit("commit")
+
+        # ensure remote exists
+        if "origin" not in [r.name for r in repo.remotes]:
+            origin = repo.create_remote("origin", url)
+        else:
+            origin = repo.remotes.origin
+            origin.set_url(url)
+
+        branch = repo.active_branch.name
+        origin.push(refspec=f"{branch}:{branch}")
+
+
 def main():
     copy_project()
+
 
 if __name__ == "__main__":
     main()
